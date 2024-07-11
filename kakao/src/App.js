58f8525b2cbd7b8f9de73b18fdf6a8523389fe73
Kyoungho Eom <kheom0512@gmail.com>
2024-07-11 16:28:26 +0900

    ㅜㅜ

--- kakao/src/App.js
+++ kakao/src/App.js
@@ -1,8 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link, Routes, Route } from 'react-router-dom';
+
+import { Header } from './components/styles/AppStyle';
+import GlobalStyle from './components/styles/GlobalStyle';
+
+import KakaoLogin from './components/pages/KakaoLogin';
+import Home from './components/pages/Home';
+import MyProfile from './components/pages/MyProfile';
+import Profile from './components/pages/Profile';
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
+
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+    <>
+      <GlobalStyle />
+      <StyledLink to="/">
+        <Header>Kakao Login Practice</Header>
+      </StyledLink>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/KakaoLogin" element={<KakaoLogin />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="/MyProfile" element={<MyProfile />} />
+      </Routes>
+    </>
   );
 }
+
 export default App;
---
+++ kakao/src/components/styles/AppStyle.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Header = styled.h1`
+text-align: left;
+color: black;
+font-size: 130px;
+font-family: helvetica;
+padding: 30px 0;
+margin: 100px 495px;
+margin-bottom: 30px;
+justify-content: center;
+cursor: pointer;
+text-shadow: 4px 4px 8px rgba(0, 0, 0, 0.5);
+
+@media screen and (max-width: 1600px) {
+  font-size: 100px;
+}
+`;
