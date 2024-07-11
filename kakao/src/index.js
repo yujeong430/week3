269738d263588ae83b73aaf861c92e434b7ce766
Kyoungho Eom <kheom0512@gmail.com>
2024-07-11 16:35:39 +0900

    ⊙︿⊙

--- kakao/src/index.js
+++ kakao/src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
-// import ReactDOM from 'react-dom'; //구버전
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App";
+import './index.css';
+import { BrowserRouter } from "react-router-dom";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
